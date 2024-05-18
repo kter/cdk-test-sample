e82b5374340dcc8f7fa6ad1f52b9dd5b96f46b37
Tomohiko Takahashi <takahashi@tomohiko.io>
2024-05-18 21:52:59 +0900

    apply prettier format

--- lib/cdk-test-sample-stack.ts
+++ lib/cdk-test-sample-stack.ts
@@ -1,26 +1,25 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-import { StateMachine, Pass } from 'aws-cdk-lib/aws-stepfunctions';
-import { Function, Runtime, Code } from 'aws-cdk-lib/aws-lambda';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import { StateMachine, Pass } from "aws-cdk-lib/aws-stepfunctions";
+import { Function, Runtime, Code } from "aws-cdk-lib/aws-lambda";
 
 export class CdkTestSampleStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id);
 
-    const stateMachine = new StateMachine(this, 'CdkTestSampleStateMachine', {
-      definition: new Pass(this, 'StartState'),
+    const stateMachine = new StateMachine(this, "CdkTestSampleStateMachine", {
+      definition: new Pass(this, "StartState"),
     });
 
-    const func = new Function(this, 'LambdaFunction', {
+    const func = new Function(this, "LambdaFunction", {
       runtime: Runtime.NODEJS_20_X,
-      handler: 'index.handler',
-      code: Code.fromAsset('lambda'),
+      handler: "index.handler",
+      code: Code.fromAsset("lambda"),
       environment: {
         STATE_MACHINE_ARN: stateMachine.stateMachineArn,
-        TEST: 'TEST1',
+        TEST: "TEST1",
       },
     });
     stateMachine.grantStartExecution(func);
-
   }
 }
--- test/cdk-test-sample.test.ts
+++ test/cdk-test-sample.test.ts
@@ -1,48 +1,46 @@
-import { App, Aspects } from 'aws-cdk-lib';
-import { Template, Match, Capture } from 'aws-cdk-lib/assertions';
-import { CdkTestSampleStack } from '../lib/cdk-test-sample-stack';
-import { AwsSolutionsChecks } from 'cdk-nag';
-import { CfnGuardValidator } from '@cdklabs/cdk-validator-cfnguard';
+import { App, Aspects } from "aws-cdk-lib";
+import { Template, Match, Capture } from "aws-cdk-lib/assertions";
+import { CdkTestSampleStack } from "../lib/cdk-test-sample-stack";
+import { AwsSolutionsChecks } from "cdk-nag";
+import { CfnGuardValidator } from "@cdklabs/cdk-validator-cfnguard";
 
-describe('CdkTestSampleStack', () => {
-  test('Synthesizes', () => {
+describe("CdkTestSampleStack", () => {
+  test("Synthesizes", () => {
     const app = new App({
-        // CloudFormation Guard
-        policyValidationBeta1: [new CfnGuardValidator()],
+      // CloudFormation Guard
+      policyValidationBeta1: [new CfnGuardValidator()],
     });
-    const stack = new CdkTestSampleStack(app, 'MyTestStack');
+    const stack = new CdkTestSampleStack(app, "MyTestStack");
     const template = Template.fromStack(stack);
     // cdk nag
     Aspects.of(stack).add(new AwsSolutionsChecks({ verbose: true }));
 
     // Faine-grained assertions Test
-    template.hasResourceProperties('AWS::Lambda::Function', {
-      Handler: 'index.handler',
-      Runtime: 'nodejs20.x',
+    template.hasResourceProperties("AWS::Lambda::Function", {
+      Handler: "index.handler",
+      Runtime: "nodejs20.x",
     });
 
     // Faine-grained assertions Test
-    template.hasResourceProperties('AWS::StepFunctions::StateMachine', {
-      DefinitionString: Match.serializedJson(
-        {
+    template.hasResourceProperties("AWS::StepFunctions::StateMachine", {
+      DefinitionString: Match.serializedJson({
         StartAt: "StartState",
         States: {
           StartState: {
             Type: "Pass",
-            End: true
-          }
-        }
-        }
-      )}),
-
-    // Snapshot Test
-    // 更新時はテストが失敗するので`npm test -- -u`コマンドを実行しスナップショットをアップデートする
-    expect(template.toJSON()).toMatchSnapshot();
+            End: true,
+          },
+        },
+      }),
+    }),
+      // Snapshot Test
+      // 更新時はテストが失敗するので`npm test -- -u`コマンドを実行しスナップショットをアップデートする
+      expect(template.toJSON()).toMatchSnapshot();
 
     // Fine-grained assertions Test: Capture
     const startAtCapture = new Capture();
     const statesCapture = new Capture();
-    template.hasResourceProperties('AWS::StepFunctions::StateMachine', {
+    template.hasResourceProperties("AWS::StepFunctions::StateMachine", {
       DefinitionString: Match.serializedJson({
         StartAt: startAtCapture,
         States: statesCapture,
@@ -54,5 +52,3 @@ describe('CdkTestSampleStack', () => {
     expect(statesCapture.asObject()).toHaveProperty(startAtCapture.asString());
   });
 });
-
-
